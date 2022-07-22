d02d16687fb6d5671d39292122210eb8a523af71
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-07-22 15:02:38 -0700

    Update index.js with changes
    Adds the logging support, moves some things around to try/catch better and uses paths instead of buffers for some file writing.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { createWriteStream, mkdirSync  } from 'node:fs';
-import { Logger } from './src/logger';
+import { Logger } from './src/logger.js';
 import { getSurveyDetails } from './src/get-survey-details.js';
 import { getSurveyResponseSchema } from './src/get-survey-response-schema.js';
 import { getSurveyResponses } from './src/get-survey-responses.js';
@@ -12,54 +12,59 @@ mkdirSync(output, { recursive: true });
 const dir = output.pathname;
 
 const logger = new Logger();
-const csvFilePath = join(dir, `${surveyId}.csv`);
-const csvDestinationStream = createWriteStream(csvFilePath);
-const csvPromise = getSurveyResponses(
-  process.env.QUALTRICS_API_TOKEN,
-  process.env.QUALTRICS_DATA_CENTER,
-  surveyId,
-  csvDestinationStream,
-  'csv',
-  logger
-);
 
-const jsonResponseFilePath = join(dir, `${surveyId}-responses.json`);
-const jsonResponseDestinationStream = createWriteStream(jsonResponseFilePath);
-const jsonResponsePromise = getSurveyResponses(
-  process.env.QUALTRICS_API_TOKEN,
-  process.env.QUALTRICS_DATA_CENTER,
-  surveyId,
-  jsonResponseDestinationStream,
-  'json',
-  logger,
-);
-
-const jsonDetailsFilePath = join(dir, `${surveyId}-survey.json`);
-const jsonDetailsDestinationStream = createWriteStream(jsonDetailsFilePath);
-const jsonDetailsPromise = getSurveyDetails(
-  process.env.QUALTRICS_API_TOKEN,
-  process.env.QUALTRICS_DATA_CENTER,
-  surveyId,
-  jsonDetailsDestinationStream,
-  logger,
-);
-
-const jsonSchemaFilePath = join(dir, `${surveyId}-schema.json`);
-const jsonSchemaDestinationStream = createWriteStream(jsonSchemaFilePath);
-const jsonSchemaPromise = getSurveyResponseSchema(
-  process.env.QUALTRICS_API_TOKEN,
-  process.env.QUALTRICS_DATA_CENTER,
-  surveyId,
-  jsonSchemaDestinationStream,
-  logger,
-);
 process.stdout.write('working...');
 try {
-  Promise.all([jsonResponsePromise, jsonDetailsPromise, jsonSchemaPromise, csvPromise]).then(() => {
-    process.stdout.write("done\n");
-  });
+  const csvFilePath = join(dir, `${surveyId}.csv`);
+  const csvDestinationStream = createWriteStream(csvFilePath);
+  const csvPromise = getSurveyResponses(
+    process.env.QUALTRICS_API_TOKEN,
+    process.env.QUALTRICS_DATA_CENTER,
+    surveyId,
+    csvDestinationStream,
+    'csv',
+    logger
+  );
+
+  const jsonResponseFilePath = join(dir, `${surveyId}-responses.json`);
+  const jsonResponseDestinationStream = createWriteStream(jsonResponseFilePath);
+  const jsonResponsePromise = getSurveyResponses(
+    process.env.QUALTRICS_API_TOKEN,
+    process.env.QUALTRICS_DATA_CENTER,
+    surveyId,
+    jsonResponseDestinationStream,
+    'json',
+    logger,
+  );
+
+  const jsonDetailsFilePath = join(dir, `${surveyId}-survey.json`);
+  const jsonDetailsPromise = getSurveyDetails(
+    process.env.QUALTRICS_API_TOKEN,
+    process.env.QUALTRICS_DATA_CENTER,
+    surveyId,
+    jsonDetailsFilePath,
+    logger,
+  );
+  const jsonSchemaFilePath = join(dir, `${surveyId}-schema.json`);
+  const jsonSchemaPromise = getSurveyResponseSchema(
+    process.env.QUALTRICS_API_TOKEN,
+    process.env.QUALTRICS_DATA_CENTER,
+    surveyId,
+    jsonSchemaFilePath,
+    logger,
+  );
+  await Promise.all([
+    jsonResponsePromise,
+    jsonDetailsPromise,
+    jsonSchemaPromise,
+    csvPromise
+  ]);
+
+  process.stdout.write("\n");
+  process.stdout.write("\ndone\n");
 } catch (e) {
   process.stderr.write("nope");
-  process.stderr.write(e);
+  process.stdout.write("\n");
   process.stderr.write(logger.getEvents().join("\n"));
+  console.log(e);
 }
